61cfa9bedffc2205aa6c6f0cd610991609c4c83a
cindyshen <cindyshen99@hotmail.com>
2012-08-17 11:57:21 -0400

    put db in code

--- purchases_example/index.php
+++ purchases_example/index.php
@@ -1,3 +1,22 @@
+<?php 
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/purchases',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
+
+?>
+
+
+
 <html>
 <body>
 <h1>Hello world</h1>
