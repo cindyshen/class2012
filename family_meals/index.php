d11cbad01b286aaef2cd50393e022ce5c1f246eb
cindyshen <cindyshen99@hotmail.com>
2012-08-20 10:08:27 -0400

    connect to database by using ActiveRecord Model

--- family_meals/index.php
+++ family_meals/index.php
@@ -1,5 +1,19 @@
 <?php
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/meals',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
 ?>
+
 <html>
 <body>
 	<p>Hello World!</p>
